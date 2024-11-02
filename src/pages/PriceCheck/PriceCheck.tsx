1a705a29108463d029aec8cf72c768ffec2c7b79
Guilherme Prigol Kramer <guilhermeprigolkramer@gmail.com>
2024-11-01 21:34:46 -0300

    verificação de preço

--- src/pages/PriceCheck/PriceCheck.tsx
+++ src/pages/PriceCheck/PriceCheck.tsx
@@ -6,6 +6,17 @@ const PriceCheck = () => {
         <div className={styles.PriceCheck}>
             <BackArrow/>
             <h1>Consulta de preço</h1>
+
+            <div className={styles.input_box}>
+                <label>Código do produto:</label>
+                <input type="number"/>
+            </div>
+
+            <h4> Descrição: </h4>
+            <p> Desodorante </p>
+
+            <h4> Valor: </h4>
+            <p>R$ 11.90</p>
         </div>
     );
 }
