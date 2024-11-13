ed025ce0f6ceb8f2145acba01fb9576e1f2281e3
Guilherme Prigol Kramer <guilhermeprigolkramer@gmail.com>
2024-11-12 21:48:35 -0300

    nao lembro

---
+++ src/apis/fake-backend.api.ts
@@ -0,0 +1,5 @@
+import products from './products.json';
+
+export async function getProducts(){
+    return products;
+}
--- src/pages/CashRegister/CashRegister.tsx
+++ src/pages/CashRegister/CashRegister.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from 'react';
 import BackArrow from '../../components/BackArrow/BackArrow';
 import styles from './CashRegister.module.scss';
+import { getProducts } from '../../apis/fake-backend.api';
 
 const CashRegister = () => {
+    const [items, setItems] = useState<any[]>([]);
+    const [products, setProducts] = useState<any>([]);
+    const [selectedItemsIndexes, setSelectedItemsIndexes] = useState<number[]>([]);
+
+    useEffect(() => {
+        async function executeGetProducts(){ setProducts(await getProducts()); }
+
+        executeGetProducts();
+    }, []);
+
     return (
         <div className={styles.CashRegister}>
             <BackArrow/>
@@ -20,19 +32,46 @@ const CashRegister = () => {
                 </div>
             </div>
 
-            <button>
+            <button onClick={() => {
+                setItems(prev => {
+                    return [
+                        ...prev,
+                        'Arroz Tio João 1Kg - Qtd: 3 - Unit: R$ 3.99 Total: R$ 11.97'
+                    ];
+                });
+            }}>
                 Adicionar à lista
             </button>
 
             <h3> Lista de produtos </h3>
+            
+            {items && items.length > 0 && (
+                <>
+                    <div className={styles.items_list}>
+                        {items.map((item, index) => {
+                            return (
+                                <div
+                                    className={`${styles.item} ${selectedItemsIndexes.includes(index) ? styles.selected : styles.not_selected}`}
+                                    key={index}
+                                    onClick={() => setSelectedItemsIndexes(prev => [...prev, index])}
+                                >
+                                    <p> {item} </p>
+                                </div>
+                            );
+                        })}
+                    </div>
 
-            <div className={styles.items_list}>
-                <div className={styles.item}> <p> Arroz Tio João 1Kg - Qtd: 3 - Unit: R$ 3.99 Total: R$ 11.97 </p> </div>
-                <div className={styles.item}> <p> Arroz Tio João 1Kg - Qtd: 3 - Unit: R$ 3.99 Total: R$ 11.97 </p> </div>
-                <div className={styles.item}> <p> Arroz Tio João 1Kg - Qtd: 3 - Unit: R$ 3.99 Total: R$ 11.97 </p> </div>
-            </div>
+                    {selectedItemsIndexes && selectedItemsIndexes.length > 0 && (
+                        <button onClick={() => setSelectedItemsIndexes([])}>
+                            Remover da lista
+                        </button>
+                    )}
+                </>
+            )}
 
-            <button> Excluir </button>
+            {!items || items.length < 1 && (
+                <p> Nenhum item adicionado à lista </p>
+            )}
 
             <div className={styles.input_container}>
                 <div className={styles.input_box}>
